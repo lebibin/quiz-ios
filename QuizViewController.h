b4a5bdb8a29c3c01fa09ff3f4ebb0744b169ee33
Kevin Candelaria <kevdashdev@gmail.com>
2014-10-14 14:06:55 +0800

    Finish Quiz application

---
+++ QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  Quiz
+//
+//  Created by Kevin Candelaria on 10/14/14.
+//  Copyright (c) 2014 Kevin Candelaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
---
+++ QuizViewController.m
@@ -0,0 +1,78 @@
+//
+//  QuizViewController.m
+//  Quiz
+//
+//  Created by Kevin Candelaria on 10/14/14.
+//  Copyright (c) 2014 Kevin Candelaria. All rights reserved.
+//
+
+#import "QuizViewController.h"
+
+@interface QuizViewController ()
+@property (nonatomic) int currentQuestionIndex;
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+@end
+
+@implementation QuizViewController
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil
+                         bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.questions = @[
+                           @"From what is cognac made?",
+                           @"What is 1 + 1?",
+                           @"What is the capital of the Philippines?"
+                           ];
+        self.answers = @[
+                         @"Grapes",
+                         @"2",
+                         @"Manila"
+                         ];
+    }
+    return self;
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showQuestion:(id)sender
+{
+    self.currentQuestionIndex++;
+    
+    if(self.currentQuestionIndex == [self.questions count]){
+        self.currentQuestionIndex = 0;
+    }
+    
+    NSString *question = self.questions[self.currentQuestionIndex];
+    
+    self.questionLabel.text = question;
+    self.answerLabel.text = @"???";
+}
+
+- (IBAction)showAnswer:(id)sender
+{
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    self.answerLabel.text = answer;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
