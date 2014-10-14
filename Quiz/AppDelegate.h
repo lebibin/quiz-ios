f20f7dd3f9780f07b4022a961313ad72a347a11f
Kevin Candelaria <kevdashdev@gmail.com>
2014-10-14 13:22:51 +0800

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Kevin Candelaria on 10/14/14.
+//  Copyright (c) 2014 Kevin Candelaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
